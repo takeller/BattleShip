5b2d0c893cbada9eb1f8e508f5d0829942003f84
Taylor Keller <takeller17@gmail.com>
2020-03-24 22:50:14 -0600

    Added methods and tests to check for a valid placement of a ship

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,65 @@
 class Board
 
+  attr_reader :board_cells
+  def initialize
+    @board_cells = {}
+  end
+
   def cells
     letters_array = [*"A".."D"]
     numbers_array = [*"1".."4"]
 
-    board_cells = {}
     letters_array.each do |letter|
       numbers_array.each do |num|
         coordinate = letter + num
-        board_cells[coordinate] = Cell.new(coordinate)
+        @board_cells[coordinate] = Cell.new(coordinate)
       end
     end
     board_cells
   end
+
+  def valid_coordinate?(coordinate)
+    return true if @board_cells.has_key?(coordinate)
+    false
+  end
+
+  def valid_placement?(ship, coordinates)
+    return false if (coordinates.length != ship.length) or (consecutive_coordinates?(coordinates) == false)
+    return false if non_diagonal_coordinates?(coordinates) == false
+    true
+  end
+
+  def consecutive_coordinates?(coordinates)
+
+    boolean_array = []
+    coordinates.each_cons(2) do |pair|
+      letter_ord_difference = pair[1][0].ord - pair[0][0].ord
+      number_ord_difference = pair[1][1].ord - pair[0][1].ord
+
+      if (letter_ord_difference == 1 || letter_ord_difference == 0) && (number_ord_difference == 1 || number_ord_difference == 0)
+        boolean_array << true
+      else
+        boolean_array << false
+      end
+    end
+
+    return false if boolean_array.any?(false)
+    true
+  end
+
+  # Both this and consecutive_coordinates can be combined into one method. By checking that the combined ordinal numbers of the coordinate pair are less than 2, I can test both that the coordinates are consecutive and non diagonal
+  def non_diagonal_coordinates?(coordinates)
+    check_diagonal_ords = []
+
+    coordinates.each_cons(2) do |pair|
+      letter_ord_difference = pair[1][0].ord - pair[0][0].ord
+      number_ord_difference = pair[1][1].ord - pair[0][1].ord
+      check_diagonal_ords << letter_ord_difference + number_ord_difference
+    end
+
+    return false if check_diagonal_ords.any? { |ord_diff_total| ord_diff_total > 1}
+    true
+  end
+
+
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -9,6 +9,8 @@ class BoardTest < Minitest::Test
 
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_exists
@@ -21,4 +23,58 @@ class BoardTest < Minitest::Test
       assert_instance_of Cell, value
     end
   end
+
+  def test_valid_coordinate?
+    @board.cells
+
+    assert_equal true, @board.valid_coordinate?("A1")
+    assert_equal true, @board.valid_coordinate?("C4")
+    assert_equal false, @board.valid_coordinate?("A5")
+    assert_equal false, @board.valid_coordinate?("E1")
+  end
+
+  def test_consecutive_coordinates?
+    @board.cells
+
+    assert_equal true, @board.consecutive_coordinates?(["A1","A2","A3"])
+    assert_equal false, @board.consecutive_coordinates?(["A1","A3"])
+    assert_equal false, @board.consecutive_coordinates?(["A2","A1"])
+    assert_equal true, @board.consecutive_coordinates?(["A1","B1"])
+    assert_equal true, @board.consecutive_coordinates?(["A1","B1", "C1", "D1"])
+    assert_equal false, @board.consecutive_coordinates?(["B1","A1", "C1", "A1"])
+  end
+
+  def test_non_diagonal_coordinates?
+    @board.cells
+
+    assert_equal true, @board.non_diagonal_coordinates?(["A1","A2","A3"])
+    assert_equal true, @board.non_diagonal_coordinates?(["A1","B1","C1"])
+    assert_equal false, @board.non_diagonal_coordinates?(["A1","B2","C3"])
+  end
+
+  def test_valid_placement_ship_length
+    @board.cells
+
+    assert_equal true, @board.valid_placement?(@cruiser, ["A1", "A2", "A3"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "A2", "A3"])
+  end
+
+  def test_valid_placement_consecutive_coordinates
+    @board.cells
+
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
+    assert_equal true, @board.valid_placement?(@submarine, ["B1", "C1"])
+  end
+  #
+  def test_valid_placement_non_diagonal_coordinates
+    @board.cells
+
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C2", "D3"])
+  end
+
 end
