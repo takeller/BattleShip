e9e0aa62a7d1f904048f2a87b33b53145a5e4200
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-03-26 17:36:07 -0600

    Add game_setup class and tests

---
+++ lib/game_setup.rb
@@ -0,0 +1,29 @@
+class GameSetup
+  def initialize
+    @board = Board.new
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @player_submarine = Ship.new("Submarine", 2)
+    @computer_submarine = Ship.new("Submarine", 2)
+  end
+
+  def main_menu
+    p "Welcome to BATTLESHIP"
+    p  "Enter p to play. Enter q to quit"
+    user_input = gets.chomp
+    until user_input == "p" || user_input == "q"
+      p  "Enter p to play. Enter q to quit"
+      user_input = gets.chomp
+    end
+    user_input
+  end
+
+  def start_game
+    exit! if main_menu == "q"
+  end
+
+  def setup
+
+  end
+
+end
